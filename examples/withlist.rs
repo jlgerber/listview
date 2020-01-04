b70dac1ddd47793cab003b7878b626c3d3ebe078
jlgerber <jlgerber@gmail.com>
2020-01-04 11:07:14 -0800

    adds hotkeys to withlist example.

--- examples/withlist.rs
+++ examples/withlist.rs
@@ -1,16 +1,44 @@
-use qt_core::QResource;
-use qt_widgets::QApplication;
 
+use qt_widgets::{QApplication, QShortcut};
+use qt_core::{Key,QResource, Slot};
 use listitem::{utility::qs, withlist::WithList};
+use qt_gui::{QKeySequence};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
     QApplication::init(|_app| unsafe {
+
         let _result = QResource::register_resource_q_string(&qs("/Users/jgerber/bin/withlist.rcc"));
 
-        let mut with_list = WithList::new();
-        with_list.set_stylesheet("/Users/jgerber/bin/withlist.qss");
+        let  with_list = Rc::new(
+            RefCell::new(
+                WithList::new()
+            )
+        );
+        let wl_c1 = with_list.clone();
+        let wl_c2 = with_list.clone();
+        with_list.borrow_mut().set_stylesheet("/Users/jgerber/bin/withlist.qss");
 
-        with_list.add_items(vec![
+        let find_slot: Slot<'static> = Slot::new(move || {
+            wl_c1.borrow_mut().set_find_mode();
+        });
+
+        let add_slot: Slot<'static> = Slot::new(move || {
+            wl_c2.borrow_mut().set_add_mode();
+        });
+
+        //let key_seq = QKeySequence::from_2_int(Key::KeyControl.to_int(), Key::KeyF.to_int());
+        //let key_seq = QKeySequence::from_int( Key::KeyF.to_int());
+       let key_seq = QKeySequence::from_q_string(&qs("Ctrl+f"));
+        let find_shortcut = QShortcut::new_2a(key_seq.as_ref(), with_list.borrow_mut().main());
+
+        //let key_seq = QKeySequence::from_2_int(Key::KeyControl.to_int(), Key::KeyA.to_int());
+        let key_seq = QKeySequence::from_q_string(&qs("Ctrl+a"));
+        //let key_seq = QKeySequence::from_int( Key::KeyA.to_int());
+        let add_shortcut = QShortcut::new_2a(key_seq.as_ref(), with_list.borrow_mut().main());
+
+        with_list.borrow_mut().add_items(vec![
             "amtools",
             "animcomp",
             "animpublish",
@@ -66,10 +94,12 @@ fn main() {
             "wambase",
             "xerces",
         ]);
+        find_shortcut.activated().connect(&find_slot);
+        add_shortcut.activated().connect(&add_slot);
 
-        with_list.item_list.borrow_mut().set_add_mode();
+        with_list.borrow_mut().item_list.borrow_mut().set_add_mode();
 
-        with_list.show();
+        with_list.borrow_mut().show();
 
         QApplication::exec()
     });
