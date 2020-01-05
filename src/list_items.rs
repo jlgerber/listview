b4d9aaf66beed88ac36dc1fc22601b4ef1253223
jlgerber <jlgerber@gmail.com>
2020-01-05 07:50:52 -0800

    adds model to ListItems struct

--- src/list_items.rs
+++ src/list_items.rs
@@ -2,23 +2,44 @@ use super::utility::qs;
 use qt_gui::{QStandardItem, QStandardItemModel};
 use qt_widgets::cpp_core::MutPtr;
 
+/// Wrapper around a Vector of QStandardItems which provides a bit of an api.
 pub struct ListItems {
     items: Vec<MutPtr<QStandardItem>>,
+    model: MutPtr<QStandardItemModel>,
 }
 
 impl ListItems {
-    pub fn new() -> Self {
-        Self { items: Vec::new() }
+    /// New up an empty ListItems instance
+    pub fn new(model: MutPtr<QStandardItemModel>) -> Self {
+        Self {
+            items: Vec::new(),
+            model,
+        }
     }
+    /// Clear the items from the list
+    ///
+    /// # Arguments
+    /// ^ None
+    ///
+    /// # Returns
+    /// None
     pub fn clear(&mut self) {
         self.items.clear();
     }
-    pub fn add_item_to(&mut self, item: &str, model: &mut MutPtr<QStandardItemModel>) {
+
+    /// Add an item to self.
+    ///
+    /// # Arguments
+    /// * iteem - The name of the item
+    ///
+    /// # Returns
+    /// * Noen
+    pub fn add_item_to(&mut self, item: &str) {
         unsafe {
             let mut si = QStandardItem::new();
             si.set_text(&qs(item));
             si.set_drop_enabled(false);
-            model.append_row_q_standard_item(si.as_mut_ptr());
+            self.model.append_row_q_standard_item(si.as_mut_ptr());
             self.items.push(si.into_ptr());
         }
     }
